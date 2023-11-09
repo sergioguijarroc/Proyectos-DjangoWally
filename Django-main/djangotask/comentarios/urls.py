a9111d16a3c439ccaa5d1456661b4189099a938b
Jesús de Dios Sánchez <jesusdediossanchez@gmail.com>
2023-11-09 13:46:14 +0100

    Cambiado nombre de carpeta de proyectos primer trimestre

--- Django-main/djangotask/comentarios/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('comentarios/', views.formulario, name='formulario'),
-    path('comentarios/confirmacion',views.confirmacion,name='confirmacion'),
-    path('comentarios/lista_comentarios',views.lista_comentarios,name='lista_comentarios'),
-
-]
---
+++ Primer-Trimestre/djangotask/comentarios/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('comentarios/', views.formulario, name='formulario'),
+    path('comentarios/confirmacion',views.confirmacion,name='confirmacion'),
+    path('comentarios/lista_comentarios',views.lista_comentarios,name='lista_comentarios'),
+  
+]
