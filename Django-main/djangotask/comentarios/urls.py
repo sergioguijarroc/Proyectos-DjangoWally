5e7c500034bf5626f3ed29931d4fabcdaec7c28a
Jesús de Dios Sánchez <jesusdediossanchez@gmail.com>
2023-11-09 13:22:01 +0100

    Terminado proyecto

--- Django-main/djangotask/comentarios/urls.py
+++ Django-main/djangotask/comentarios/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('comentarios/', views.comentarios, name='comentarios'),
-    path('comentarios/',views.confirmacion,name='confirmacion')
+    path('comentarios/', views.formulario, name='formulario'),
+    path('comentarios/confirmacion',views.confirmacion,name='confirmacion'),
+    path('comentarios/lista_comentarios',views.lista_comentarios,name='lista_comentarios'),
+
 ]
--- Django-main/djangotask/comentarios/views.py
+++ Django-main/djangotask/comentarios/views.py
@@ -1,12 +1,12 @@
-from .forms import ComentariosForm
 from django.shortcuts import render,redirect
+from django.contrib import messages
+from .forms import ComentariosForm
 from .models import Comentarios
 '''
 Importar formulario
 '''
 
-def comentarios(request):
-    comentarios = Comentarios.objects.all()
+def formulario(request):
     '''
     if=Si el formulario está para enviarlo con el metodo post 
     else=Si el formulario es nuevo se crea otro formulario vacío
@@ -18,10 +18,16 @@ def comentarios(request):
             '''
             Redirect confirmacion nuevo formulario ir a la vista de confirmacion
             '''
+            print("Redirigiendo a la página de confirmación...")
             return redirect('confirmacion')
     else:
         form=ComentariosForm()
-    return render(request, 'comentarios/base.html', {'comentarios': comentarios,'form':form})
+    return render(request, 'comentarios/base.html', {'form':form})
 # Create your views here.
+
 def confirmacion(request):
-    return render(request,'comentarios/confirmacion.html')
+    mensaje="Gracias por tu comentario"
+    return render(request,'comentarios/confirmacion.html',{'mensaje':mensaje})
+def lista_comentarios(request):
+    lista= Comentarios.objects.all()
+    return render(request,'comentarios/lista_comentarios.html',{'lista':lista})
